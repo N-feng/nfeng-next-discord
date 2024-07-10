cbfc8f90cdc2485c654678cfc7acd48b14bc955d
N-feng <308561157@qq.com>
2024-07-10 23:06:23 +0800

    feat✨: chat message done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@c
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { cn } from "@/lib/utils";
+import { SocketProvider } from "@/components/providers/socket.provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -32,8 +34,12 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              <QueryProvider>
+                {children}
+              </QueryProvider>
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
